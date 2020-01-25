f44cbbfa3b8e20057fd7177ff7dbfadc5415805c
MinexAutomation <minex.automation@gmail.com>
2020-01-25 02:00:50 -0800

    Added IValueEqualityComparer<T> service definition interface type.

--- source/R5T.Vandalia.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Vandalia.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Vandalia.Base/Code/Services/Definitions/IValueEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Vandalia
+{
+    /// <summary>
+    /// Allows comparing the equality of objects.
+    /// Basically, this is <see cref="System.Collections.Generic.IEqualityComparer{T}"/> without the requirement to implement GetHashCode().
+    /// </summary>
+    public interface IValueEqualityComparer<T>
+    {
+        bool Equals(T x, T y);
+    }
+}
